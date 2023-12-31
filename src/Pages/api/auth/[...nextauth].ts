c8b89ca8c9d2c0b6800a0fe9a21c54dd39b92963
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2023-12-31 23:18:33 +0400

    Try googl day 3

--- src/Pages/api/auth/[...nextauth].ts
+++ src/Pages/api/auth/[...nextauth].ts
@@ -1,22 +1,20 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth, {NextAuthOptions} from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
-const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: 'offline',
-          response_type: 'code'
-        }
-      }
-    }),
-    // You can add other providers here if needed in the future
-  ],
-  // You can also add custom callbacks, pages, etc. here as per your requirements
-};
+export const authOptions: NextAuthOptions ={
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID as string,
+            clientSecret: process.env.GOOGLE_SECRET as string,
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: 'offline',
+                    response_type: 'code'
+                }
+            }
+        }),
+    ]
+}
 
 export default NextAuth(authOptions);
