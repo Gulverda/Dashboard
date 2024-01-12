5abbea89a698ecb0679487cbdd54d26ed192ac64
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2024-01-12 14:21:25 +0400

    Update index.tsx

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Login from '@/components/Login/Login'
+import Login from '@/components/Login'
 import Header from '@/components/Header'
 import SideMenu from '@/components/SideMenu/SideMenu';
 import Dashboard from '@/pages/dashboard';
